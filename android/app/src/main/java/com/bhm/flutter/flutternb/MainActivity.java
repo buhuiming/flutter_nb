2554e5fcdb88e7aef2a5f5274b2c83a78f6fea0f
buhuiming <873247376@qq.com>
2019-01-17 16:54:35 +0800

    update

--- android/app/src/main/java/com/bhm/flutter/flutternb/MainActivity.java
+++ android/app/src/main/java/com/bhm/flutter/flutternb/MainActivity.java
@@ -1,13 +1,26 @@
 package com.bhm.flutter.flutternb;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    setStatus();
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+  }
+
+  private void setStatus(){
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);//窗口透明的状态栏
+      requestWindowFeature(Window.FEATURE_NO_TITLE);//隐藏标题栏
+      getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);//窗口透明的导航栏
+    }
   }
 }
---
+++ lib/ui/page/splash_page.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+}
+
+class _SplashPageState extends State<SplashPage>{
+
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
